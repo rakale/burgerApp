f5cb79597af62e828f4561d6b860fd89b2bc0a19
Raiko Kalev <rakale@ttu.ee>
2021-03-31 15:26:47 +0300

    adding a separate checkout page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -92,29 +92,30 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        //alert('You continue!');
-        this.setState({loading:true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Raiko Kalev',
-                address:{
-                    street: 'Keldrim2e 2',
-                    zipCode: 10113,
-                    country: 'EST'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-        .then(response=>{
-            this.setState({loading:false, purchasing:false});
-        })
-        .catch(error=>{
-            this.setState({ loading: false, purchasing: false });
-        })
+        // //alert('You continue!');
+        // this.setState({loading:true});
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Raiko Kalev',
+        //         address:{
+        //             street: 'Keldrim2e 2',
+        //             zipCode: 10113,
+        //             country: 'EST'
+        //         },
+        //         email: 'test@test.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
+        // axios.post('/orders.json', order)
+        // .then(response=>{
+        //     this.setState({loading:false, purchasing:false});
+        // })
+        // .catch(error=>{
+        //     this.setState({ loading: false, purchasing: false });
+        // })
+        this.props.history.push('./checkout')
     }
 
     render () {

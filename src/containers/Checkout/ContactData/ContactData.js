596f3427d9ca028489fe4d06fb389ea9a8bf3427
Raiko Kalev <rakale@ttu.ee>
2021-04-01 15:41:31 +0300

    adding a dropdown component, video 239done

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,14 +4,61 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
 import axios from '../../../axiosOrders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state={
-        name:'',
-        email:'',
-        address:{
-            street:'',
-            postalCode:''
+        orderForm:{
+            name: {
+                elementType:'input',
+                elementConfig:{
+                    type:'text',
+                    placeholder:'Your name'
+                },
+                value:''
+            },
+            street: {
+                elementType:'input',
+                elementConfig:{
+                    type:'text',
+                    placeholder:'Street'
+                },
+                value:''
+            },
+            zipCode: {
+                elementType:'input',
+                elementConfig:{
+                    type:'text',
+                    placeholder:'ZipCode'
+                },
+                value:''
+            },
+            country: {
+                elementType:'input',
+                elementConfig:{
+                    type:'text',
+                    placeholder:'Country'
+                },
+                value:''
+            },
+            email: {
+                elementType:'input',
+                elementConfig:{
+                    type:'email',
+                    placeholder:'Your E-Mail'
+                },
+                value:''
+            },
+            deliveryMethod: {
+                elementType:'select',
+                elementConfig:{
+                    options:[
+                        {value:'fastest',displayValue:'Fastest'},
+                        {value:'cheapest',displayValue:'Cheapest'}
+                    ]
+                },
+                value:''
+            }
         },
         loading:false
     }
@@ -22,16 +69,7 @@ class ContactData extends Component {
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            customer: {
-                name: 'Raiko Kalev',
-                address:{
-                    street: 'Keldrim2e 2',
-                    zipCode: 10113,
-                    country: 'EST'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
+            
         }
         axios.post('/orders.json', order)
         .then(response=>{
@@ -44,12 +82,24 @@ class ContactData extends Component {
     }
 
     render(){
+
+        const formElementsArray=[];
+        for(let key in this.state.orderForm){
+            formElementsArray.push({
+                id:key,
+                config:this.state.orderForm[key]
+            });
+        }
+
         let form = (
             <form>
-            <input className={classes.Input} type="text" name="name" placeholder="Your Name"/>
-            <input className={classes.Input} type="email" name="email" placeholder="Your Mail"/>
-            <input className={classes.Input} type="text" name="street" placeholder="Street"/>
-            <input className={classes.Input} type="text" name="postal" placeholder="Postal Code"/>
+            {formElementsArray.map(formElement=>(
+                <Input 
+                key={formElement.id}
+                elementType={formElement.config.elementType}
+                elementConfig={formElement.config.elementConfig}
+                value={formElement.config.value}/>
+            ))}
             <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
         </form>
         );

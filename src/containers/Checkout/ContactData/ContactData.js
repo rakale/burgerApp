73e1ee2aa9e99f1b6debbf784d9239bfba128862
Raiko Kalev <rakale@ttu.ee>
2021-04-05 12:22:39 +0300

    vid 240 laptop

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -81,6 +81,20 @@ class ContactData extends Component {
         })
     }
 
+    inputChangedHandler = (event,inputIdendifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        }
+
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdendifier]
+        };
+
+        updatedFormElement.value = event.target.value;
+        updatedOrderForm[inputIdendifier] = updatedFormElement;
+        this.setState({orderForm:updatedOrderForm});
+    }
+
     render(){
 
         const formElementsArray=[];
@@ -98,7 +112,8 @@ class ContactData extends Component {
                 key={formElement.id}
                 elementType={formElement.config.elementType}
                 elementConfig={formElement.config.elementConfig}
-                value={formElement.config.value}/>
+                value={formElement.config.value}
+                changed={(event)=>this.inputChangedHandler(event, formElement.id)}/>
             ))}
             <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
         </form>
